f176f44dc687030f09dad6a1afdad73a4ffc0707
arunprasanna008 <arunprasanna008@gmail.com>
2021-06-03 17:13:51 -0700

    its done

--- lc_e_two_sum.py
+++ lc_e_two_sum.py
@@ -1,17 +1,30 @@
 def verify(actual_output, expected_output):
-        try:
-            assert (actual_output == expected_output)
-        except AssertionError:
-            print ("wrong answer")
+    try:
+        assert (actual_output == expected_output)
+        print("correct solution %s" % actual_output)
+    except AssertionError:
+        print ("wrong solution \n actual: %s \n expected: %s" % (actual_output, expected_output))
 
 class Solution:
-    def twosum(self, nums, target, expected_output):
-        print(nums, target)
+    def twoSum(self, nums, target, expected_output):
+        actual_output = []
         for index, num in enumerate(nums):
-            print(index, num, expected_output)
-            v = [1,1]
-        verify(v,expected_output)
+            if (target-num) in nums and nums.index(target-num) != index:
+                if nums.index(target-num) > index:
+                    actual_output = [index, nums.index(target-num)]
+                else:
+                    actual_output = [nums.index(target-num), index]
+                verify(actual_output,expected_output)
+                break
+            else:
+                if index == (len(nums)-1):
+                    print("The sum of any two items from the list %s won't make the target %s" %(nums,target))
+                
             
         
 my_object = Solution()
-my_object.twosum(['2','7','3'],9,[0,1])
+my_object.twoSum(nums=[2,7,3],target=5,expected_output=[0,2])
+my_object.twoSum(nums=[3,4],target=7,expected_output=[0,1])
+my_object.twoSum(nums=[3,6,9,1,2,4],target=7,expected_output=[0,5])
+my_object.twoSum(nums=[3,4,1,1,1,3,2],target=6,expected_output=[1,6])
+my_object.twoSum(nums=[3,3,1,1,1,1,1,3,2],target=6,expected_output=[0,1])
